6fc0d26e522374f43db091468ae4f52566cc8a79
dolenkoplant <dolenkoplant@gmail.com>
2016-05-08 18:18:13 +0300

    test3

--- src/main/java/modeles/Teacher.java
+++ src/main/java/modeles/Teacher.java
@@ -1,9 +1,19 @@
 package modeles;
 
 public class Teacher  extends User{
-	private final String USERTYPE = "Teacher";
+	private String USERTYPE = "Teacher";
+
+	public String getUSERTYPE() {
+		return USERTYPE;
+	}
+
+	public void setUSERTYPE(String USERTYPE) {
+		this.USERTYPE = USERTYPE;
+	}
 
 	public Teacher(String email, String username, String password, String firstname, String lastname) {
 		super(email, username, password, firstname, lastname);
 	}
+
+
 }
