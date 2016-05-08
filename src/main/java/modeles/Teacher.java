616af9fac14d26c6516f73e4c3762d2c4835022d
dolenkoplant <dolenkoplant@gmail.com>
2016-05-08 18:15:40 +0300

    test3

--- src/main/java/modeles/Teacher.java
+++ src/main/java/modeles/Teacher.java
@@ -1,7 +1,7 @@
 package modeles;
 
 public class Teacher  extends User{
-	private static final String USERTYPE = "Teacher";
+	private final String USERTYPE = "Teacher";
 
 	public Teacher(String email, String username, String password, String firstname, String lastname) {
 		super(email, username, password, firstname, lastname);
