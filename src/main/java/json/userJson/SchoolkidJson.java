4de28a28e16b473a764c651453188b297b87c147
dolenkoplant <dolenkoplant@gmail.com>
2016-05-10 03:15:26 +0300

    test3

--- src/main/java/json/userJson/SchoolkidJson.java
+++ src/main/java/json/userJson/SchoolkidJson.java
@@ -9,10 +9,21 @@ import java.io.Serializable;
  */
 public class SchoolkidJson extends UserJson implements Serializable {
 
+    private String classNumber;
+
     public SchoolkidJson(){}
 
     public SchoolkidJson(Integer id, String email, String username, String password, String firstname, String lastname, String classNumber) {
         super(id, email, username, password, firstname, lastname, classNumber);
+        this.setClassNumber(classNumber);
         role = "schoolkid";
+    }
+
+    public String getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(String classNumber) {
+        this.classNumber = classNumber;
     }
 }
