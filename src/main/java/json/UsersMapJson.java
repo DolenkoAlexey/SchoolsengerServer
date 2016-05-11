2ace68f4397f3840f2276022dee5906cc5571807
dolenkoplant <dolenkoplant@gmail.com>
2016-05-11 17:26:34 +0300

    hhh

--- src/main/java/json/UsersMapJson.java
+++ src/main/java/json/UsersMapJson.java
@@ -8,8 +8,10 @@ import java.util.Map;
 import com.google.gson.annotations.SerializedName;
 
 import json.userJson.SchoolkidJson;
+import json.userJson.SuperadminJson;
 import json.userJson.TeacherJson;
 import json.userJson.UserJson;
+import modeles.Schoolkid;
 import modeles.Superadmin;
 import modeles.User;
 
@@ -21,9 +23,9 @@ public class UsersMapJson implements Serializable{
 	
 	public UsersMapJson(Map<Class, List<? extends UserJson>> users) {
 		this.users = new HashMap<>();
-		this.users.put(SchoolkidJson.class, users.get(SchoolkidJson.class));
-		this.users.put(TeacherJson.class, users.get(TeacherJson.class));
-		this.users.put(Superadmin.class, users.get(Superadmin.class));
+		this.users.put(SchoolkidJson.class, (List<SchoolkidJson>)users.get(SchoolkidJson.class));
+		this.users.put(TeacherJson.class, (List<TeacherJson>)users.get(TeacherJson.class));
+		this.users.put(SuperadminJson.class, (List<SuperadminJson>)users.get(SuperadminJson.class));
 	}
 
 	public Map<Class, List<? extends UserJson>> getMessages() {
@@ -31,8 +33,8 @@ public class UsersMapJson implements Serializable{
 	}
 
 	public void setMessages(Map<Class, List<? extends UserJson>> users) {
-		this.users.put(SchoolkidJson.class, users.get(SchoolkidJson.class));
-		this.users.put(TeacherJson.class, users.get(TeacherJson.class));
-		this.users.put(Superadmin.class, users.get(Superadmin.class));
+		this.users.put(SchoolkidJson.class, (List<SchoolkidJson>)users.get(SchoolkidJson.class));
+		this.users.put(TeacherJson.class, (List<TeacherJson>)users.get(TeacherJson.class));
+		this.users.put(SuperadminJson.class, (List<SuperadminJson>)users.get(SuperadminJson.class));
 	}
 }
