5ccaa6ade7ba90f7ae77694e97b8c9f5c8f14499
dolenkoplant <dolenkoplant@gmail.com>
2016-05-11 17:04:31 +0300

    hhh

--- src/main/java/json/UsersMapJson.java
+++ src/main/java/json/UsersMapJson.java
@@ -1,12 +1,16 @@
 package json;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.google.gson.annotations.SerializedName;
 
+import json.userJson.SchoolkidJson;
+import json.userJson.TeacherJson;
 import json.userJson.UserJson;
+import modeles.Superadmin;
 import modeles.User;
 
 
@@ -16,7 +20,10 @@ public class UsersMapJson implements Serializable{
 	private Map<Class, List<? extends UserJson>> users;
 	
 	public UsersMapJson(Map<Class, List<? extends UserJson>> users) {
-		this.users = users;
+		users = new HashMap<>();
+		users.put(SchoolkidJson.class, users.get(SchoolkidJson.class));
+		users.put(TeacherJson.class, users.get(TeacherJson.class));
+		users.put(Superadmin.class, users.get(Superadmin.class));
 	}
 
 	public Map<Class, List<? extends UserJson>> getMessages() {
@@ -24,6 +31,8 @@ public class UsersMapJson implements Serializable{
 	}
 
 	public void setMessages(Map<Class, List<? extends UserJson>> users) {
-		this.users = users;
+		users.put(SchoolkidJson.class, users.get(SchoolkidJson.class));
+		users.put(TeacherJson.class, users.get(TeacherJson.class));
+		users.put(Superadmin.class, users.get(Superadmin.class));
 	}
 }
