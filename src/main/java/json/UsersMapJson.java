8e534fd6e6227c6769a2a5ec911e9a640720f923
dolenkoplant <dolenkoplant@gmail.com>
2016-05-11 17:34:01 +0300

    hhh

--- src/main/java/json/UsersMapJson.java
+++ src/main/java/json/UsersMapJson.java
@@ -1,12 +1,16 @@
 package json;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.google.gson.annotations.SerializedName;
 
+import entities.SchoolkidEntity;
+import entities.SuperadminEntity;
+import entities.TeacherEntity;
 import json.userJson.SchoolkidJson;
 import json.userJson.SuperadminJson;
 import json.userJson.TeacherJson;
@@ -23,9 +27,31 @@ public class UsersMapJson implements Serializable{
 	
 	public UsersMapJson(Map<Class, List<? extends UserJson>> users) {
 		this.users = new HashMap<>();
-		this.users.put(SchoolkidJson.class, (List<SchoolkidJson>)users.get(SchoolkidJson.class));
-		this.users.put(TeacherJson.class, (List<TeacherJson>)users.get(TeacherJson.class));
-		this.users.put(SuperadminJson.class, (List<SuperadminJson>)users.get(SuperadminJson.class));
+
+		List<SchoolkidJson> schoolkidJsons = (List<SchoolkidJson>) users.get(SchoolkidJson.class);
+		List<TeacherJson> teacherJsons = (List<TeacherJson>) users.get(TeacherJson.class);
+		List<SuperadminJson> superadminJsons = (List<SuperadminJson>) users.get(SuperadminJson.class);
+
+		List<SchoolkidJson> schoolkidsJson = new ArrayList<>();
+		List<TeacherJson> teachersJson = new ArrayList<>();
+		List<SuperadminJson> superadminsJson = new ArrayList<>();
+
+		if(schoolkidJsons != null)
+			for (SchoolkidJson entity: schoolkidJsons) {
+				schoolkidsJson.add(entity);
+			}
+		if(teacherJsons != null)
+			for (TeacherJson entity: teacherJsons) {
+				teachersJson.add(entity);
+			}
+		if(superadminJsons != null)
+			for (SuperadminJson entity: superadminJsons) {
+				superadminsJson.add(entity);
+			}
+
+		this.users.put(SchoolkidJson.class, schoolkidsJson);
+		this.users.put(TeacherJson.class, teachersJson);
+		this.users.put(SuperadminJson.class, superadminsJson);
 	}
 
 	public Map<Class, List<? extends UserJson>> getMessages() {
