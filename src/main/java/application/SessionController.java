00592663c6b7d9c98ed73e829774f4b2e186d745
dolenkoplant <dolenkoplant@gmail.com>
2016-05-11 19:54:44 +0300

    hhh

--- src/main/java/application/SessionController.java
+++ src/main/java/application/SessionController.java
@@ -2,6 +2,7 @@ package application;
 
 
 
+import org.neo4j.cypher.internal.compiler.v2_0.ast.In;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +15,14 @@ import service.MessageDAOService;
 import service.UserDAO;
 import service.UserDAOService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/session")
 public class SessionController {
 	
-	@RequestMapping(method=RequestMethod.GET, value="/getinterlocutors")
+	@RequestMapping(method=RequestMethod.GET, value="/interlocutors")
 	public String getInterlocutors(@RequestParam(value="id") Integer idFrom) {
 	
 		MessageDAO messageService = new MessageDAOService();
@@ -26,7 +30,11 @@ public class SessionController {
 		
 //		List<Integer> idsTo = messageService.selectIdsToByIdFrom(idFrom);
 //		UsersDataMapJson usersDataList = userService.getUsersDataListByIds(idsTo);
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
 		
-		return new GsonBuilder().create().toJson("usersDataList");
+		return new GsonBuilder().create().toJson(userService.getUsersDataListByIds(list));
 	}
 }
